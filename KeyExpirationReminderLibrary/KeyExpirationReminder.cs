942196c91fb844099d0986c76ca88ed7adb9567e
Hamed Vatani <hamedv@gmail.com>
2022-03-01 12:31:48 +0330

    done

---
+++ KeyExpirationReminderLibrary/KeyExpirationReminder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace KeyExpirationReminderLibrary
+{
+    public class KeyExpirationReminder:IDisposable
+    {
+        internal class BagItem
+        {
+            public object Tag { get; set; }
+            public int Timeout { get; set; }
+            public DateTime TimeStamp { get; set; }
+            public Action<string, object> Callback { get; set; }
+        }
+
+        private readonly ConcurrentDictionary<string, BagItem> dictionary;
+        private readonly Timer timer;
+
+        public KeyExpirationReminder()
+        {
+            dictionary = new ConcurrentDictionary<string, BagItem>();
+            timer = new Timer(500);
+            timer.Elapsed += timer_elapsed;
+            timer.Start();
+        }
+
+        public bool TryAddKey(string key, int miliseconTimeout, object tag, Action<string, object> callback)
+        {
+            return dictionary.TryAdd(key, new BagItem
+            {
+                Timeout = miliseconTimeout,
+                Tag = tag,
+                TimeStamp = DateTime.Now,
+                Callback = callback
+            });
+        }
+
+        public bool TryAddKey(string key, int miliseconTimeout, Action<string, object> callback)
+        {
+            return TryAddKey(key, miliseconTimeout, null, callback);
+        }
+
+        private void timer_elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (dictionary.Count == 0)
+                return;
+            timer.Stop();
+
+            foreach (var item in dictionary)
+                if (item.Value.TimeStamp.AddMilliseconds(item.Value.Timeout) <= DateTime.Now &&
+                    dictionary.TryRemove(item.Key, out var value))
+                    Task.Run(() => { value.Callback(item.Key, value.Tag); });
+
+            timer.Start();
+        }
+
+        public void Dispose()
+        {
+            timer?.Close();
+        }
+    }
+}
--- KeyExpirationReminderTester/Program.cs
+++ KeyExpirationReminderTester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using KeyExpirationReminderLibrary;
 
 namespace KeyExpirationReminderTester
 {
@@ -6,7 +8,13 @@ namespace KeyExpirationReminderTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reminder = new KeyExpirationReminder();
+            reminder.TryAddKey("Key 1", 5000, (k, t) => { Console.WriteLine("Key Expiration : " + k); });
+            reminder.TryAddKey("Key 2", 4000, (k, t) => { Console.WriteLine("Key Expiration : " + k); });
+            reminder.TryAddKey("Key 3", 6000, (k, t) => { Console.WriteLine("Key Expiration : " + k); });
+            reminder.TryAddKey("Key 4", 1500, (k, t) => { Console.WriteLine("Key Expiration : " + k); });
+
+            Console.ReadKey();
         }
     }
 }
